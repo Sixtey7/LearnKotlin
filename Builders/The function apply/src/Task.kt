eb645b48a5ace4542d79be5d1d8f3c4b466c2e3c
Patrick Shaw <sixtey7@gmail.com>
2019-07-18 22:09:09 -0400

    working through Builders

--- Builders/The function apply/src/Task.kt
+++ Builders/The function apply/src/Task.kt
@@ -1,4 +1,9 @@
-fun <T> T.myApply(f: T.() -> Unit): T { TODO() }
+fun <T> T.myApply(f: T.() -> Unit): T {
+    f();
+
+    return this;
+
+}
 
 fun createString(): String {
     return StringBuilder().myApply {
